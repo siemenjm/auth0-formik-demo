334d8280c546b9e09f8d2da4adf23337414d8994
Jared Siemen <siemenjm@gmail.com>
2023-01-04 11:24:53 -0500

    Style Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
-      <div className="upperHeader">
+      <div className={styles.upperHeader}>
         <h2>Not signed in</h2>
-        <button type="submit">Sign In</button>
+        <button className={styles.signButton} type="submit">
+          Sign In
+        </button>
       </div>
-      <nav>
+      <nav className={styles.lowerHeader}>
         <ul>
           <li>
-            <Link to={'/'}>Home Page</Link>
+            <Link to={'/'} className={styles.navLink}>
+              Home Page
+            </Link>
           </li>
           <li>
-            <Link to={'unprotected-page'}>Unprotected Page</Link>
+            <Link to={'unprotected-page'} className={styles.navLink}>
+              Unprotected Page
+            </Link>
           </li>
           <li>
-            <Link to={'protected-page'}>Protected Page</Link>
+            <Link to={'protected-page'} className={styles.navLink}>
+              Protected Page
+            </Link>
           </li>
         </ul>
       </nav>
