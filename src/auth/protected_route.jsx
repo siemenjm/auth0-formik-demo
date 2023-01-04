27ca7f4c66893daba8e32a984472b93997075fa5
Jared Siemen <siemenjm@gmail.com>
2023-01-04 17:00:56 -0500

    Try fixing issure where Routes requires Route

--- src/auth/protected_route.jsx
+++ src/auth/protected_route.jsx
@@ -1,13 +1,22 @@
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 import { Route } from 'react-router-dom';
 
-export default function ProtectedRoute({ element, ...args }) {
-  return (
-    <Route
-      element={withAuthenticationRequired(element, {
-        onRedirecting: () => <h2>Loading...</h2>,
-      })}
-      {...args}
-    />
-  );
-}
+// export default function ProtectedRoute({ element, ...args }) {
+//   return (
+//     <Route
+//       element={withAuthenticationRequired(element, {
+//         onRedirecting: () => <h2>Loading...</h2>,
+//       })}
+//       {...args}
+//     />
+//   );
+// }
+
+export const ProtectedRoute = ({ element, ...args }) => (
+  <Route
+    element={withAuthenticationRequired(element, {
+      onRedirecting: () => <h2>Loading...</h2>,
+    })}
+    {...args}
+  />
+);
