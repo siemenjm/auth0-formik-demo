fcb9bab7c449362d6dda92f3ddedcc430cb802f3
Jared Siemen <siemenjm@gmail.com>
2023-01-04 17:01:23 -0500

    Remove unused import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
 import Auth0ProviderWithHistory from './auth/auth0_provider_with_history';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
