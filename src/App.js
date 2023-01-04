3b339e79034dfd83307d55404b608dc09b602426
Jared Siemen <siemenjm@gmail.com>
2023-01-04 10:53:13 -0500

    Add protected and unprotected pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import Protected from './pages/Protected';
+import Unprotected from './pages/Unprotected';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="protected-page" element={<Protected />} />
+        <Route path="unprotected-page" element={<Unprotected />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/Unprotected.jsx
@@ -0,0 +1,3 @@
+export default function Unprotected() {
+  return <div>Unprotected Page</div>;
+}
